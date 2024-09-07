de9f625a249ac69846ee670b6fcb0a4c02ab06f2
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-09-07 11:28:07 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
-import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+    
     </div>
   );
 }
