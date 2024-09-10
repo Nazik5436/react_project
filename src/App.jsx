9c46306b0e0c4ad8e0cd05199fed9cebe0a26c6e
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-09-10 20:30:39 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { List } from "./components/List/List";
 import { Cond } from "./components/Cond/Cond";
 import { Request } from "./components/Request/Request";
-
+import { List1 } from "./components/Domaska/List1";
+ 
 function App() {
   return (
     <div className="App">
+      <List1></List1>
       <List></List>
       <Cond></Cond>
       <hr />
       <Request></Request>
+
     </div>
   );
 }
