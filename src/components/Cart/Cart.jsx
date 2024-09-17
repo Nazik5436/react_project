7aec527cd68b6d0a3d8cfcd6873bfd103431f716
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-09-17 15:27:19 +0300

    1

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,18 @@
-
+import "./Cart.scss"
 
 export const Cart = () => {
 
     return(
-        <div>
-            Its Cart
+        <div className="contacts">
+            <h1 className="title">Plan your visit !</h1>
+            <p className="con__par">Our zoo is open to visitors every day from 9:00 a.m. to 6:00 p.m. We are located in a picturesque park where cafes, souvenir shops and playgrounds are available for you. We offer convenient parking and excellent public transport to make your visit comfortable and pleasant.</p>
+            <p className="con__par">Address: Prirodna Street, 123, City, Country</p>
+            <p className="con__par">How to find us: We are located only a 10-minute drive from the city center. You can reach us by car or bus #45.</p>
+            <ul>
+                <li>Phone: (123) 456-7890</li>
+                <li>Email: info@zoo.com</li>
+            </ul>
+            <h2>Call to Action: Contact us to book a tour or with any questions. We look forward to seeing you!</h2>
         </div>
     )   
 };
--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -1,10 +1,58 @@
 import "./Likes.scss"
+import asian_elephants from "/NAZAR/IT/GitHub/react_project/src/components/Img/Asian elephants.jpg" 
+import red_pandas from "/NAZAR/IT/GitHub/react_project/src/components/Img/28138502587_a0a020ae9a_k.jpeg"
+import african_lions from "/NAZAR/IT/GitHub/react_project/src/components/Img/AfricanLionJasiri_resize2019.jpg"
+import logo from "/NAZAR/IT/GitHub/react_project/src/components/Img/logo.jpg"
+import daf from "/NAZAR/IT/GitHub/react_project/src/components/Img/1614752068-feeding_1_desktop.jpg"
+import daf1 from "/NAZAR/IT/GitHub/react_project/src/components/Img/animal-feeding-elephant-listing-900x600.jpg"
+import daf2 from "/NAZAR/IT/GitHub/react_project/src/components/Img/_111497983_sarah-janefeedingscarlettheredpandaparadiseparkcornwall.jpg"
+
+
 
 export const Likes = () => {
 
     return(
         <div className="animals__osnova">
-            Its List
+
+            <article className="article">
+                <div>
+                    <h1 className="anm__title">Meet our residents</h1>
+                    <p className="anm__par">Amazing animals from different parts of the world live in our zoo - from mighty lions and tigers to graceful giraffes and playful monkeys. We have sections for savannas, rainforests, deserts and oceans so you can see animals in a close to natural environment.</p>
+                </div>
+                <div>
+                    <img src={logo} alt="logo" className="logo_img"/>
+                </div>
+            </article>
+            
+            <ul>
+                <h2 className="anm__second__title">Some of our favorites:</h2>
+                <li className="li"></li>
+                <li className="li">
+                    <h3 className="anm__third__title">Asian elephants are the largest land animals, they amaze with their strength and intelligence.</h3>
+                    <img src={asian_elephants} alt="Asian elephants" className="anm__img"/></li>
+                <li className="li">
+                    <h3 className="anm__third__title">Rare red pandas are charming inhabitants of tropical forests.</h3>
+                    <img src={red_pandas} alt="Red pandas" className="anm__img"/>
+                </li>
+                <li className="li">
+                    <h3 className="anm__third__title">African lions are the kings of the savannah, mesmerizing with their majesty.</h3>
+                    <img src={african_lions} alt="African lions" className="anm__img"/>
+                </li>
+            </ul>
+            <p className="anm__par">Don't miss our daily animal feeding and special shows where you can get up close and personal with our pets!</p>
+            <article className="second__article">
+                <img src={daf} alt="daf" className="daf"/>
+                <img src={daf1} alt="daf" className="daf"/>
+                <img src={daf2} alt="daf" className="daf"/>
+            </article>
+            <article className="thiird__article">
+                <div>
+                    <p className="anm__par">See the full list of animals and their activity schedule!</p>
+                </div>
+                <div>
+                    <button className="anm_but">Full list</button>
+                </div>
+            </article>
         </div>
     )   
 };
