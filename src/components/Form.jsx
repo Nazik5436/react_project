8241495a43cae277b447dee506bdf5205af6e27a
Nazik5436 <166996612+Nazik5436@users.noreply.github.com>
2024-09-29 12:12:26 +0300

    1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,14 @@ const validationScheme = Yup.object().shape({
         .required('Mail is required'),
     phone: Yup.string()
         .required('The phone is wired')
-        .matches(/^[0-9]{10}$/, 'Phone number must contain 10 digits')
+        .matches(/^[0-9]{10}$/, 'Phone number must contain 10 digits'),
+    password: Yup.string()
+        .min(6, 'Пароль має містити щонайменше 6 символів')
+        .required('Обов’язкове поле'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Паролі повинні співпадати')
+        .required('Обов’язкове поле'),
+    
 })
 
 export const FormComponent = () => {
@@ -43,14 +50,14 @@ export const FormComponent = () => {
                         </div>
                         
                         <div className="second__item">
-                            <label htmlFor="name">Emal id :</label>
+                            <label htmlFor="email">Emal id :</label>
                             <br/>
                             <Field type="email" id="email" name="email" placeholder="info@xyz.com" className="block"/>
                             <ErrorMessage name="email" component="div" className="error"></ErrorMessage>
                         </div>
                         
                         <div className="third__item">
-                            <label htmlFor="name">Password :</label>
+                            <label htmlFor="password">Password :</label>
                             <br/>
                             <Field type="password" id="password" name="password" placeholder="xxxxxxxxxx" className="block"/>
                             <ErrorMessage name="password" component="div" className="error"></ErrorMessage>
@@ -62,12 +69,26 @@ export const FormComponent = () => {
                             <Field type="text" id="company" name="company" placeholder="Enter your name" className="block"/>                        
                         </div>
                         
-                        <div className="thir__item">
-                            <label htmlFor="name">Mobile No.  :</label>
+                        <div className="fifth__item">
+                            <label htmlFor="phone">Mobile No.  :</label>
                             <br/>
-                            <Field type="text" id="phone" name="phone" placeholder="+380" className="block"/>
+                            <Field type="text" id="phone" name="phone" placeholder="+38" className="block"/>
                             <ErrorMessage name="phone" component="div" className="error"></ErrorMessage>
                             <br/>
+                        </div>
+
+                        <div className="sixth__item">
+                            <label htmlFor="name">Last name :</label>
+                            <br/>
+                            <Field type="text" id="last_name" name="last_name" placeholder="Enter your name" className="block"/>
+                            <ErrorMessage name="last_name" component="div" className="error"></ErrorMessage>
+                        </div>
+
+                        <div className="seventh__item">
+                            <label htmlFor="confirmPassword">Confirm Password  :</label>
+                            <br/>
+                            <Field type="password" id="confirmPassword" name="confirmPassword" placeholder="xxxxxxxxxx" className="block"/>
+                            <ErrorMessage name="confirmPassword" component="div" className="error"></ErrorMessage>
                         </div>
                         
                         <div className="but__item">
